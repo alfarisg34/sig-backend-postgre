86e336efffe50e1944e8df3a77a195b75be42fed
Alfarisg 34 <alfarisg34@gmail.com>
2022-06-29 14:51:09 +0700

    calculation

---
+++ controller/calc.js
@@ -0,0 +1,54 @@
+const {KebudayaanModel,ProvinsiModel } = require('../models')
+const { sequelize } = require('./models')
+const math = require('mathjs');
+
+module.exports.getCalculation = async function (req, res) {
+  try {
+    const dataBudaya = await KebudayaanModel.findAll();
+    const dataProvinsi = await ProvinsiModel.findAll();
+    const totalIndividual = await KebudayaanModel.findAll({
+      attributes: [[sequelize.fn('count', '*'), 'totalBudaya']],
+      include: [
+        {
+          model: ProvinsiModel
+        }
+      ],
+      group: ['Provinsi.id']
+    });
+
+    let arrTotal = [];
+    totalIndividual.forEach(item => {
+      arrTotal.push(parseInt(item.dataValues.totalBudaya) + 1);
+    });
+
+    const stdev = math.std(arrTotal);
+
+    const totalBudaya = dataBudaya.length + 33;
+    const totalProvinsi = dataProvinsi.length;
+    const average = totalBudaya / totalProvinsi;
+
+    const n = req.params.multiplier;
+    const low = average - (n * stdev);
+    const high = average + (n * stdev);
+
+    const dataCalculate = {
+      total: totalBudaya,
+      totalProvinsi,
+      average,
+      stdev,
+      low,
+      high
+    }
+    return res.status(200).json({
+      success: true,
+      message: 'success',
+      data: dataCalculate
+    })
+  } catch (error) {
+    return res.status(400).json({
+      sucess: false,
+      error: error,
+      message: error.message
+    })
+  }
+}
